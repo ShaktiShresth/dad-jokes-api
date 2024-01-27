1969abf8ade8c0814c1f734949e6594e398f69e0
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-01-27 15:37:20 +0545

    dad jokes api

---
+++ index.js
@@ -0,0 +1,21 @@
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+// async await
+const generateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await fetch("https://icanhazdadjoke.com", setHeader);
+    const data = await res.json();
+    jokes.innerHTML = data.joke;
+  } catch (error) {
+    console.log(`The error is ${err}`);
+  }
+};
+
+jokeBtn.addEventListener("click", generateJokes);
+generateJokes();
